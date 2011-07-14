e64a2459e6c742aae05f27b959b752e07206f490
stefanfoulis <stefan.foulis@gmail.com>
2011-07-14 18:49:22 +0200

    massive refactor for consistency

--- notifyme_by_email/delivery_backends.py
+++ notifyme_by_email/delivery_backends.py
@@ -1,9 +1,9 @@
 #-*- coding: utf-8 -*-
 from django.core.mail.message import EmailMultiAlternatives
-from notifyme.delivery.base import BaseDeliveryBackend
 from django.conf import settings
 from django.template.loader import render_to_string
-from django.core.mail import send_mail
+from notifyme.delivery_backends.base import BaseDeliveryBackend
+
 
 class EmailBackend(BaseDeliveryBackend):
     identifier = 'email'
@@ -14,35 +14,33 @@ class EmailBackend(BaseDeliveryBackend):
     def get_from_email(self, user=None):
         return settings.DEFAULT_FROM_EMAIL
 
-    def deliver_to(self, user, context, notice, language):
+    def deliver_to(self, user, context, notification, language):
         to_email = self.get_email(user)
         context.update({
             'to_email': to_email,
         })
         subject = render_to_string(
             (
-                "notifyme/notices/%s/email/subject.txt" % notice.identifier,
-                "notifyme/notices/generic/email/subject.txt",
+                "notifyme/notification_types/%s/email/subject.txt" % notification.identifier,
+                "notifyme/notification_types/generic/email/subject.txt",
             ),
             context_instance=context
         )
         body = render_to_string(
             (
-                "notifyme/notices/%s/email/body.txt" % notice.identifier,
-                "notifyme/notices/generic/email/body.txt",
+                "notifyme/notification_types/%s/email/body.txt" % notification.identifier,
+                "notifyme/notification_types/generic/email/body.txt",
             ),
             context_instance=context
         )
         body_html = render_to_string(
             (
-                "notifyme/notices/%s/email/body.html" % notice.identifier,
-                "notifyme/notices/generic/email/body.html",
+                "notifyme/notification_types/%s/email/body.html" % notification.identifier,
+                "notifyme/notification_types/generic/email/body.html",
             ),
             context_instance=context
         )
-        # TODO: send html
-        send_mail(subject, body, self.get_from_email(user), [to_email])
-
+        # send the HTML Email
         email_msg = EmailMultiAlternatives(subject, body, self.get_from_email(user),
             [to_email], headers={})
         email_msg.attach_alternative(body_html, "text/html")
